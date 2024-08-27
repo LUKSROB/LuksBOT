e1a932ad86cc4ab8bccbd7a6b2f64165d10be698
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2024-08-27 01:08:31 -0300

    add .gitignore and update file messages.js

--- utils/functions/messages.js
+++ utils/functions/messages.js
@@ -1,3 +1,11 @@
 // Randomize messages to be sent to the user
 
 const {  } = require("discord.js");
+
+module.exports = async (interaction) => {
+    const random = Math.random() * 3;
+
+    interaction
+        .reply(random)
+        .catch(console.error);
+}
