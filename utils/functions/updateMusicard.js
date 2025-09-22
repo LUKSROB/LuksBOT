41220b2fffdbcce65224fa8b8ee133175b284b1a
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-22 00:54:25 -0300

    ADd function of update musicard

---
+++ utils/functions/updateMusicard.js
@@ -0,0 +1,35 @@
+// Function to generate and update a music card image for the currently playing track
+
+// Import necessary modules
+const { Classic } = require("musicard");
+const { convertTime, musicProgress } = require("../../utils/functions/convertTime");
+
+// Function to update the music card with current track info and progress
+async function updateMusicard(track, player) {
+    const musicLength = convertTime(track.info.length);
+    const timeProgress = convertTime(player.position);
+
+    const percProgress = musicProgress(player.position, track.info.length);
+
+    const musicard = await Classic({
+        thumbnailImage: track.info.thumbnail,
+        backgroundColor: '#070707',
+        progress: percProgress,
+        progressColor: '#FF7A00',
+        progressBarColor: '#5F2D00',
+        name: track.info.title,
+        nameColor: '#FF7A00',
+        author: track.info.author,
+        authorColor: '#696969',
+        startTime: timeProgress,
+        endTime: musicLength,
+        timeColor: '#FF7A00',
+    });
+
+    return musicard;
+}
+
+// Export the function for use in other modules
+module.exports = {
+    updateMusicard
+};
