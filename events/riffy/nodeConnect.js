4e48f7223ab3bb1e954bf70ac1eef64231d28481
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-22 00:52:48 -0300

    add nodeConect event of riffy

---
+++ events/riffy/nodeConnect.js
@@ -0,0 +1,8 @@
+// Event handler for Riffy node connect event
+
+// Export the node connect event handler
+module.exports = async (node) => {
+
+    console.log(`Node "${node.name}" connected.`);
+
+}
