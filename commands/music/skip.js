24394f052db66e8420f7bae8aed76292ad751a3c
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-24 18:49:00 -0300

    Add command skip

---
+++ commands/music/skip.js
@@ -0,0 +1,31 @@
+// Command skip: Skip the current playing track
+
+// Import necessary modules
+const { SlashCommandBuilder } = require("discord.js");
+const { skip } = require("../../utils/functions/music");
+
+// Export the skip command module
+module.exports = {
+    // Define the command structure
+	data: new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription("Skip the current playing track"),
+    // Execute the command
+    execute: async ( interaction ) => {
+
+        const { member, client } = interaction;
+
+        const player = client.riffy.players.get(interaction.guild.id);
+
+        if (!player) {
+            return interaction.reply({ content: "No estoy reproduciendo nada actualmente.", flags: 64 });
+        }
+
+        if (player.queue.size === 0) {
+            return interaction.reply({ content: "La cola está vacía.", flags: 64 });
+        };
+
+        skip(interaction, player);
+
+    }
+};
