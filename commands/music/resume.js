1bb00a2792c7964e0bd15270c888cc7e2364aaa0
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-24 18:49:10 -0300

    Add command resume

---
+++ commands/music/resume.js
@@ -0,0 +1,40 @@
+// Command loop: Loop the current song or playlist
+
+// Import necessary modules
+const { SlashCommandBuilder } = require('discord.js');
+const { resume } = require('../../utils/functions/music');
+
+// Export the pause command module
+module.exports = {
+    // Define the command structure
+    data: new SlashCommandBuilder()
+        .setName('loop')
+        .setDescription('Reproduce la canción o la lista de reproducción en un bucle')
+        .addStringOption(option =>
+            option.setName('mode')
+                .setDescription('Selecciona el modo de bucle')
+                .setChoices(
+                    { name: 'Canción', value: 'track' },
+                    { name: 'Lista de reproducción', value: 'queue' },
+                    { name: 'Desactivar', value: 'none' }
+                )
+        ),
+    // Execute the command
+    execute: async (interaction) => {
+        const { guild, client } = interaction;
+        const mode = interaction.options.getString('mode')
+        
+        let player = client.riffy.players.get(guild.id);
+
+        try {
+
+            resume(interaction, player);
+
+        } catch (error) {
+
+            console.error(error);
+            interaction.reply({ content: 'Error al cambiar el estado de bucle ❌', flags: 64 });
+        }
+
+    }
+}
