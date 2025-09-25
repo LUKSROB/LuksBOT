c40f6f8fe800c1de8ebbf221ff10983ed6d6d8be
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-24 22:44:11 -0300

    Add buttons to controle songs

--- events/riffy/trackStart.js
+++ events/riffy/trackStart.js
@@ -3,13 +3,59 @@
 // Import necessary modules
 const { AttachmentBuilder } = require("discord.js");
 const { updateMusicard } = require("../../utils/canvas/updateMusicard");
+const { PALETTE_RAPIDAPI } = require("../../config.json")
 
 // Export the track start event handler
 module.exports = async (player, track, payload, client) => {
 
+    const buttons = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+            .setCustomId('stop')
+            .setStyle(4)
+            .setEmoji(`⏹️`),
+        new ButtonBuilder()
+            .setCustomId('loop')
+            .setStyle(2)
+            .setEmoji('🔁'),
+        new ButtonBuilder()
+            .setCustomId('pause')
+            .setStyle(2)
+            .setEmoji('⏯️'),
+        new ButtonBuilder()
+            .setCustomId('skip')
+            .setStyle(2)
+            .setEmoji('⏭️'),
+        new ButtonBuilder()
+            .setCustomId('queue')
+            .setStyle(2)
+            .setEmoji('📃')
+    )
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'x-rapidapi-key': PALETTE_RAPIDAPI.rapidapi_key,
+            'x-rapidapi-host': 'color-detection-from-image.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            imageUrl: track.rawData.info.artworkUrl
+        })
+    };
+
+    const { url } = PALETTE_RAPIDAPI;
+    let color = "#"
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        color += result?.accentColor || 'FF7A00';
+    } catch (error) {
+        console.error(error);
+    }
+
     const channel = client?.channels?.cache?.get(player.textChannel);
-    
-    const musicard = await updateMusicard(track, player, true);
+
+    const musicard = await updateMusicard(track, player, true, color);
 
     client.user.setActivity({
         name: track.info.title,
@@ -21,12 +67,12 @@ module.exports = async (player, track, payload, client) => {
 
     if (musicard) {
         const attachment = new AttachmentBuilder(musicard, { name: 'musicard.png' });
-        musicardMessage = await channel.send({ files: [attachment] });
+        musicardMessage = await channel.send({ files: [attachment], components: [buttons] });
     }
 
     player.musicInterval = setInterval(async () => {
         try {
-            const updatedMusicard = await updateMusicard(track, player);
+            const updatedMusicard = await updateMusicard(track, player, false, color);
             if (updatedMusicard) {
                 const updatedAttachment = new AttachmentBuilder(updatedMusicard, { name: 'musicard.png' });
                 await musicardMessage.edit({ files: [updatedAttachment] });
