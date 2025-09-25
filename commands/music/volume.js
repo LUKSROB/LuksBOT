23efd2538788530e4e66bca53a28e8771a8df228
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-24 23:41:25 -0300

    Add volume command

---
+++ commands/music/volume.js
@@ -0,0 +1,41 @@
+
+
+
+const { SlashCommandBuilder } = require('discord.js');
+const { volume } = require('../../utils/functions/music');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('volume')
+        .setDescription('Ajusta el volumen de la música.')
+        .addNumberOption(option =>
+            option.setName('nivel')
+                .setDescription('El nivel de volumen (0-100)')
+                .setRequired(true)
+                .setChoices(
+                    { name: '10', value: 10 },
+                    { name: '20', value: 20 },
+                    { name: '30', value: 30 },
+                    { name: '40', value: 40 },
+                    { name: '50', value: 50 },
+                    { name: '60', value: 60 },
+                    { name: '70', value: 70 },
+                    { name: '80', value: 80 },
+                    { name: '90', value: 90 },
+                    { name: '100', value: 100 }
+                )
+            ),
+    execute: async (interaction) => {
+
+        const player = interaction.client.riffy.players.get(interaction.guild.id);
+
+        if (!player || !player.playing) {
+            return interaction.reply({ content: '❌ No hay música reproduciéndose.', flags: 64 });
+        
+        }
+
+        const volumeLevel = interaction.options.getNumber('nivel');
+
+        volume( interaction, player, volumeLevel);
+    }
+}
