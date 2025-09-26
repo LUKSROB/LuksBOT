7268aa55a92976c27922b0096ff7e346c514da60
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-26 17:57:54 -0300

    Add brightness function in musicard

--- utils/canvas/updateMusicard.js
+++ utils/canvas/updateMusicard.js
@@ -3,20 +3,21 @@
 // Import necessary modules
 const { Classic } = require("musicard");
 const { convertTime, musicProgress } = require("../../utils/functions/convertTime");
+const { brightnessHex } = require("../functions/colors");
 
 // Function to create or update the music card
 async function updateMusicard(track, player, init = false, color) {
     const musicLength = convertTime(track.info.length);
     const timeProgress = convertTime(player.position);
-
     const percProgress = musicProgress(player.position, track.info.length);
+    const colorBright = brightnessHex(color || '#FF7A00', 0.3);
 
     const musicard = await Classic({
         thumbnailImage: track.info.thumbnail,
         backgroundColor: '#070707',
         progress: init ? 0 : percProgress,
         progressColor: color || '#FF7A00',
-        progressBarColor: color + '33' || '#FF7A0033',
+        progressBarColor: colorBright,
         name: track.info.title,
         nameColor: color || '#FF7A00',
         author: track.info.author,
