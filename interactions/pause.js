1b20a304b988bfe9f9b9f71fe21590406ef60d33
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-26 17:53:12 -0300

    Add async functions

--- interactions/pause.js
+++ interactions/pause.js
@@ -9,11 +9,11 @@ module.exports = async (interaction) => {
         
     let player = client.riffy.players.get(guild.id);
 
-    interaction.deferReply();
+    await interaction.deferReply();
 
     try {
         if (!player || !guild.members.me.voice.channel) {
-            return interaction.editReply({ content: 'No estoy reproduciendo nada actualmente.', flags: 64 });
+            return await interaction.editReply({ content: 'No estoy reproduciendo nada actualmente.', flags: 64 });
         }
 
         await pause(interaction, player);
