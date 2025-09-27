61d08378f1577ffe7321d967574f51b97cbdbdf5
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2025-09-27 01:29:33 -0300

    Adapt function generate color image with more colors

--- utils/canvas/colorImage.js
+++ utils/canvas/colorImage.js
@@ -4,11 +4,21 @@
 const { createCanvas } = require('@napi-rs/canvas');
 
 // Function to generate a image filled with a specified hex color
-function generateColorImage(hexColor) {
-    const canvas = createCanvas(64, 64);
+function generateColorImage(palette, colorCount = 1) {
+    const width = 64
+    let height = 0
+    colorCount === 1 ? height = 64 : height = 16;
+    const canvas = createCanvas(width , height * colorCount);
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = hexColor;
-    ctx.fillRect(0, 0, 64, 64);
+    if (colorCount === 1) {
+        ctx.fillStyle = palette;
+        ctx.fillRect(0, 0, width, height);
+    } else {
+        for (let i = 0; i < colorCount; i++) {
+            ctx.fillStyle = palette[i];
+            ctx.fillRect(0, i * height, width, height);
+        }
+    } 
     return canvas.toBuffer("image/png");
 }
 
