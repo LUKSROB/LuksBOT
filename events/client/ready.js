fbf318a390e3aa6da794831ff301ae74769e232d
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2026-03-01 02:07:36 -0300

    .

---
+++ events/client/ready.js
@@ -0,0 +1,16 @@
+
+
+const { updateCache } = require('../../utils/functions/invites')
+const { setGuild } = require('../../db/guild')
+
+module.exports = (client) => {
+    client.riffy.init(client.user.id);
+    console.log(`Connected to Discord! as ${client.user.username}`);
+
+    client.guilds.cache.map(async guild => {
+        await setGuild(guild);
+    });
+
+    updateCache(client);
+    
+};
