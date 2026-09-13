5f85edf282c60782eda3b14e7c43898963ce40da
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2026-09-13 15:48:41 -0300

    Add check of commands not created

--- events/client/messageCreate.js
+++ events/client/messageCreate.js
@@ -3,15 +3,19 @@
 // Export the messageCreate event handler
 module.exports = async (message) => {
     if (message.author.bot) return;
-    
-    if (!message.content.startsWith("/")) {
+
+    if (message.content.startsWith("/")) {
         const args = message.content.slice(1).split(" ")[0];
 
+        const client = message.client;
+        const command = client.commands.get(args);
+
         try {
-            const command = require(`../commands/${args}.js`);
             command.run(message);
         } catch (error) {
-            console.log(`this command is not created yet: ${args}`, error.message);
+            console.log(`Este comando no existe: ${args}`, error.message);
+
+            message.reply(`El comando \`${args}\` no fue creado aun, \n si desea solicitarlo, por favor contacte al administrador.`, { ephemeral: true });
         }
     }
 }
