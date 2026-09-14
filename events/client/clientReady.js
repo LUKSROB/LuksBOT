bd2cf1cfc50e92f0050437cc7d34a5d1dbb53098
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2026-09-14 17:04:13 -0300

    Add async function in clientReady

--- events/client/clientReady.js
+++ events/client/clientReady.js
@@ -1,9 +1,9 @@
-
+// Event handler for when the client is ready
 
 const { updateCache } = require('../../utils/functions/invites')
 const { setGuild } = require('../../db/guild')
 
-module.exports = (client) => {
+module.exports = async (client) => {
     client.riffy.init(client.user.id);
     console.log(`Connected to Discord! as ${client.user.username}`);
 
