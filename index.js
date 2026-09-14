d4477fb200bd8e119e779655d20df1d54304b8d8
Luksrob <133062299+LUKSROB@users.noreply.github.com>
2026-09-14 18:15:50 -0300

    Add all in a block async function

--- index.js
+++ index.js
@@ -22,129 +22,136 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Create a new Discord client
-const client = new Discord.Client({
-    intents: 53608447
-});
-
-// Create Riffy instance
-const nodes = config.NODES;
-
-client.riffy = new Riffy(client, nodes, {
-    send: (payload) => {
-        const guild = client.guilds.cache.get(payload.d.guild_id);
-        if (guild) guild.shard.send(payload);
-    },
-    defaultSearchPlatform: "ytmsearch",
-    restVersion: "v4",
-    bypassChecks: {
-        nodeFetchInfo: true // bypass strict node info validation to allow next-track playback
-    }
-});
-
-// Connect to MongoDB 
-(async () => {
-    try {
-        await mongoose.connect(config.MONGO_URL);
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Error connecting to Mongoose:', err);
-    }
-})();
-
-// Set up DeepL translator
-client.translator = new deepl.Translator(config.DeepL_API_KEY);
-console.log('DeepL Translator initialized')
-
-// Load commands
-client.commands = new Discord.Collection();
-
-fs.readdirSync('./commands')
-    .forEach((commandfile) => {
-        const cmd = fs.readdirSync(`./commands/${commandfile}`);
-
-        for (const file of cmd) {
-            const command = require(`./commands/${commandfile}/${file}`);
-
-            client.commands.set(command.data.name, command);
-        }
-    });
-
-// Charge commands
-const REST = new Discord.REST().setToken(config.BOT_TOKEN);
+console.clear();
 
 (async () => {
-    try {
-        await REST.put(
-            Discord.Routes.applicationGuildCommands(config.APP_ID, config.GUILD_ID),
-            { body: client.commands.map(cmd => cmd.data.toJSON()) }
-        );
-        console.log(`Loaded ${client.commands.size} commands`);
-    } catch (err) {
-        console.log('Error loading commands');
-        console.error(err);
-    }
-})();
+    // Create a new Discord client
+    const client = await new Discord.Client({
+        intents: 53608447
+    });
 
-// Load Client events
-fs.readdirSync('./events/client')
-    .filter((filename) => filename.endsWith('.js'))
-    .forEach((filename) => {
-        try {
-            const listener = require(`./events/client/${filename}`);
-            const eventName = path.basename(filename, '.js');
+    // Create Riffy instance
+    const nodes = config.NODES;
 
-            client.on(eventName, (...args) => listener(...args, client));
-        } catch (err) {
-            console.log(`Error loading event ${filename}`, err);
+    client.riffy = new Riffy(client, nodes, {
+        send: (payload) => {
+            const guild = client.guilds.cache.get(payload.d.guild_id);
+            if (guild) guild.shard.send(payload);
+        },
+        defaultSearchPlatform: "ytmsearch",
+        restVersion: "v4",
+        bypassChecks: {
+            nodeFetchInfo: true
         }
     });
 
-// Load Riffy events
-fs.readdirSync('./events/riffy')
-    .filter((filename) => filename.endsWith('.js'))
-    .forEach((filename) => {
+
+    // Connect to MongoDB 
+    (async () => {
         try {
-            const listener = require(`./events/riffy/${filename}`);
-            const eventName = path.basename(filename, '.js');
-
-            client.riffy.on(eventName, (...args) => listener(...args, client));
+            await mongoose.connect(config.MONGO_URL);
+            console.log('Connected to MongoDB');
         } catch (err) {
-            console.log(`Error loading event ${filename}`, err);
+            console.error('Error connecting to Mongoose:', err);
         }
-    });
+    })();
 
-// Event handler for raw event
-client.on("raw", (d) => {
-    if (![Discord.GatewayDispatchEvents.VoiceStateUpdate, Discord.GatewayDispatchEvents.VoiceServerUpdate,].includes(d.t)) return;
-    client.riffy.updateVoiceState(d);
-});
+    // Set up DeepL translator
+    client.translator = new deepl.Translator(config.DeepL_API_KEY);
+    console.log('DeepL Translator initialized')
 
-// Express server for health check
-app.get('/', (req, res) => {
-    res.send(config.WEB)
+    // Load commands
+    client.commands = new Discord.Collection();
 
-    const fullUrl = req.get('host') + req.originalUrl;
+    fs.readdirSync('./commands')
+        .forEach((commandfile) => {
+            const cmd = fs.readdirSync(`./commands/${commandfile}`);
 
-    if (client.isReady()) {
-        setTimeout(() => {
-            client.user.setActivity({
-                name: fullUrl,
-                type: 3,
-                status: 'online',
-            });
-        }, 1000);
-    }
-});
+            for (const file of cmd) {
+                const command = require(`./commands/${commandfile}/${file}`);
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-
-    client.login(config.BOT_TOKEN)
-        .then(() => console.log('Bot logged in to Discord successfully!'))
-        .catch(err => {
-            console.error('ERROR: Failed to login to Discord:', err);
-            process.exit(1);
+                client.commands.set(command.data.name, command);
+            }
         });
-});
 
+    // Charge commands
+    const REST = new Discord.REST().setToken(config.BOT_TOKEN);
+
+    (async () => {
+        try {
+            await REST.put(
+                Discord.Routes.applicationGuildCommands(config.APP_ID, config.GUILD_ID),
+                { body: client.commands.map(cmd => cmd.data.toJSON()) }
+            );
+            console.log(`Loaded ${client.commands.size} commands`);
+        } catch (err) {
+            console.log('Error loading commands');
+            console.error(err);
+        }
+    })();
+
+    // Load Client events
+    fs.readdirSync('./events/client')
+        .filter((filename) => filename.endsWith('.js'))
+        .forEach((filename) => {
+            try {
+                const listener = require(`./events/client/${filename}`);
+                const eventName = path.basename(filename, '.js');
+
+                client.on(eventName, (...args) => listener(...args, client));
+            } catch (err) {
+                console.log(`Error loading event ${filename}`, err);
+            }
+        });
+
+    // Load Riffy events
+    fs.readdirSync('./events/riffy')
+        .filter((filename) => filename.endsWith('.js'))
+        .forEach((filename) => {
+            try {
+                const listener = require(`./events/riffy/${filename}`);
+                const eventName = path.basename(filename, '.js');
+
+                client.riffy.on(eventName, (...args) => listener(...args, client));
+            } catch (err) {
+                console.log(`Error loading event ${filename}`, err);
+            }
+        });
+
+    // Event handler for raw event
+    client.on("raw", (d) => {
+        if (![
+            Discord.GatewayDispatchEvents.VoiceStateUpdate,
+            Discord.GatewayDispatchEvents.VoiceServerUpdate
+        ].includes(d.t)) return;
+        client.riffy.updateVoiceState(d);
+    });
+
+    // Express server for health check
+    app.get('/', (req, res) => {
+        res.send(config.WEB)
+
+        const fullUrl = req.get('host') + req.originalUrl;
+
+        if (client.isReady()) {
+            setTimeout(() => {
+                client.user.setActivity({
+                    name: fullUrl,
+                    type: 3,
+                    status: 'online',
+                });
+            }, 1000);
+        }
+    });
+
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+
+        client.login(config.BOT_TOKEN)
+            .then(() => console.log('Bot logged in to Discord successfully!'))
+            .catch(err => {
+                console.error('ERROR: Failed to login to Discord:', err);
+                process.exit(1);
+            });
+    });
+})();
